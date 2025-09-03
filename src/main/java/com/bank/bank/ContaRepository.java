b67be431dd9f8dff95e4145a13cb1ccd4066328f
JorgeLucasON <jorgelucasolivx@gmail.com>
2025-09-03 09:10:57 -0300

    import org.springframework.data.jpa.repository.JpaRepository;

---
+++ src/main/java/com/bank/bank/ContaRepository.java
@@ -0,0 +1,14 @@
+package com.bank.bank;
+
+import com.bank.bank.Conta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ContaRepository extends JpaRepository<Conta, Long> {
+    Optional<Conta> findByNumeroConta(String numeroConta);
+    List<Conta> findByTitularContaining(String titular);
+}
