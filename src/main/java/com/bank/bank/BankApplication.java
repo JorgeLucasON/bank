ddd72216a6fa9b0f1b893fd08c1fe00b536fec68
JorgeLucasON <jorgelucasolivx@gmail.com>
2025-09-03 12:00:44 -0300

    fix: Por conta do meu .env não esta dando para iniciar a aplicação entao precisamos adicionar uma nova dependencia para conseguir roda a aplicação e tambem manter as gredencias seguras entao tivemos que colocar na raiz do projeto.

--- src/main/java/com/bank/bank/BankApplication.java
+++ src/main/java/com/bank/bank/BankApplication.java
@@ -1,5 +1,5 @@
 package com.bank.bank;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BankApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
 		SpringApplication.run(BankApplication.class, args);
 	}
 
